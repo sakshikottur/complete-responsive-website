cdf570c3fbeb851e6133614601137f95163386a0
sakshikottur <sakshikottur.cse.rymec@gmail.com>
2024-02-24 14:45:28 +0530

    init

---
+++ home.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+
+
+
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+    <!--header section starts here-->
+
+    <section class="header">
+
+        <a href="home.php" class="logo">BEATBOX</a>
+
+        <nav class="navbar">
+            <a href="home.php">HOME</a>
+            <a href="about.php">ABOUT</a>
+            <a href="services.php">SERVICES</a>
+            <a href="gallary.php">GALLARY</a>
+            <a href="book.php">BOOK</a>
+
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars">
+
+        </div>
+
+    </section>
+
+    <!--header section ends here-->
+
+    <!--services section starts here-->
+
+    <section class="service">
+
+        <h1 class="heading-title"> our services </h1>
+
+        <div class="box-container">
+
+            <div class="box">
+                <img src="images/icon-1.png" alt="" style="height: 100px;">
+                <h3>MUSIC</h3>
+            </div>
+
+            <div class="box">
+                <img src="images/icon-2.png" alt="" style="height: 100px;">
+                <h3>TICKETS</h3>
+            </div>
+
+            <div class="box">
+                <img src="images/icon-3.png" alt="" style="height: 100px;">
+                <h3>FOOD</h3>
+            </div>
+
+            <div class="box">
+                <img src="images/icon-4.png" alt="" style="height: 100px;">
+                <h3>DRINKS</h3>
+            </div>
+
+
+
+        </div>
+
+    </section>
+
+    <!--services section ends here-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- footer section starts -->
+    <section class="footer">
+
+        <div class="box-container">
+
+            <div class="box">
+                <h3>QUICK LINKS</h3>
+                <a href="home.php"> <i class="fas fa-angle-right"></i> HOME</a>
+                <a href="about.php"> <i class="fas fa-angle-right"></i> ABOUT</a>
+                <a href="services.php"> <i class="fas fa-angle-right"></i> SERVICES</a>
+                <a href="gallary.php"> <i class="fas fa-angle-right"></i> GALLARY</a>
+                <a href="book.php"> <i class="fas fa-angle-right"></i> BOOK</a>
+            </div>
+
+            <div class="box">
+                <h3>EXTRA LINKS</h3>
+                <a href="#"> <i class="fas fa-angle-right"></i> ASK QUESTIONS</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> ABOUT US</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> PRIVACY POLICY</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> TERMS OF USE</a>
+
+            </div>
+
+            <div class="box">
+                <h3>CONTACT INFO</h3>
+                <a href="#"> <i class="fas fa-phone"></i> +123-456-7890</a>
+                <a href="#"> <i class="fas fa-phone"></i> +111-222-3333</a>
+                <a href="#"> <i class="fas fa-envelope"></i> harshitha@gmail.com</a>
+                <a href="#"> <i class="fas fa-envelope"></i> sakshi@gmail.com</a>
+                <a href="#"> <i class="fas fa-map"></i> BALLARI, KARNATAKA - 583104</a>
+
+
+            </div>
+
+            <div class="box">
+                <h3>FOLLOW US</h3>
+                <a href="#"> <i class="fab fa-facebook-f"></i>facebook</a>
+                <a href="#"> <i class="fab fa-twitter"></i>twitter</a>
+                <a href="#"> <i class="fab fa-instagram"></i>instagram</a>
+                <a href="#"> <i class="fab fa-linkedin"></i>Linkedin</a>
+
+            </div>
+
+        </div>
+
+        <div class="credit"> created by <span>B.H.HARSHITHA AND SAKSHI.K</span> | all rights reserved! </div>
+
+    </section>
+
+
+    <!-- footer section ends -->
+
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+
+    <script src="js/script.js">
+
+    </script>
+
+</body>
+
+</html>
