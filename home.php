8f7c6025996f4f2f83443831a9e6bf30f0b63b0c
harshitha1130 <harshithabh.cse.rymec@gmail.com>
2024-02-26 23:05:55 +0530

    check2

--- home.php
+++ home.php
@@ -143,27 +143,73 @@
 
     <section class="home-packages">
 
-        <h1 class="heading">
+        <h1 class="heading-title">our packages</h1>
 
             <div class="box-container">
 
                 <div class="box">
 
                     <div class="image">
-                        <img src="images/img-1.jpg" alt="">
+                        <img src="images\img-1.jpg" alt="">
                     </div>
                     <div class="content">
-                        <h3></h3>
+                        <h3>adventure & musics</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur vitae quibusdam laboriosam ipsam voluptatem fugit quasi quae suscipit iusto architecto iure odio voluptates facilis, cumque incidunt labore eaque deleniti. Labore.</p>
+                        <a href="book.php" class="btn">book now</a>
                     </div>
                 </div>
 
-            </div>
+            <div class="box-container">
+               
+                <div class="box">
 
-        </h1>
+                    <div class="image">
+                        <img src="images\img-4.jpg" alt="">
+                    </div>
+                    <div class="content">
+                        <h3>adventure & musics</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur vitae quibusdam laboriosam ipsam voluptatem fugit quasi quae suscipit iusto architecto iure odio voluptates facilis, cumque incidunt labore eaque deleniti. Labore.</p>
+                        <a href="book.php" class="btn">book now</a>
+                    </div>
+                </div>
+
+            <div class="box-container">
+
+                <div class="box">
+
+                    <div class="image">
+                     <img src="images\img-2.jpg" alt="" >
+                    </div>
+                    <div class="content">
+                        <h3>adventure & musics</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur vitae quibusdam laboriosam ipsam voluptatem fugit quasi quae suscipit iusto architecto iure odio voluptates facilis, cumque incidunt labore eaque deleniti. Labore.</p>
+                        <a href="book.php" class="btn">book now</a>
+                    </div>
+                </div>
+            
+
+       
+    </div>
+
+    <div class="load-more"> <a href="book.php" class="btn">load more</a></div>
+
 
     </section>
 
     <!--home packages section ends here-->
+
+    <!--home offer section starts here--> 
+
+    <section class="home-offer">
+        <div class="content">
+            <h3>upto 50% off !!!</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur temporibus ex dignissimos blanditiis minima, perferendis quia et qui necessitatibus similique.</p>
+            <a href="book.php" class="btn">book now</a>
+        </div>
+    </section>
+
+
+     <!--home offer section ends here-->
 
 
 
